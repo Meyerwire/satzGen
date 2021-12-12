7f0d53406b7bce27356caa38ac22c1e1f1d42aaf
Meyer <jeremymakoben@yahoo.de>
2021-12-12 20:54:30 +0100

    finished window and added json

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import tkinter as tk
 from random import randint
 
+window = tk.Tk()
 
+window.title = "Zufällige Sätze"
+
+window.geometry("600x400")
+newlabel = tk.Label(text = "Satz Generator")
+
+newlabel.grid(column=0,row=0)
+window.mainloop()
